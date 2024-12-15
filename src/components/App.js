d2539e19c251cd6154d7bb5b009d48a2c508d4fe
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-15 05:56:31 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,59 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [id, setId] = useState(3);
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([
+    {
+      todo: "Eat",
+      id: 1
+    },
+    {
+      todo: "sleep",
+      id: 2
+    }
+  ])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodo([...todo, { todo: input, id: id }]);
+
+    setId(id+1);
+  }
+
+  function deleteTodo(e, id) {
+    setTodo(currTodo => {
+      return currTodo.filter(todo => todo.id !== id);
+    })
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h3>To-Do List</h3>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={input} onChange={e => setInput(e.target.value)} required />
+            <button>Add Todo</button>
+          </form>
+        </div>
+        <ul>
+          {
+            todo.map(item => {
+              return (
+                <li key={item.id}>
+                  <div>{item.todo}</div>
+                  <button onClick={e => deleteTodo(e, item.id)}>Delete</button>
+                  <br /><br />
+                </li>
+              )
+            })
+          }
+        </ul>
     </div>
   )
 }
 
-export default App
+export default App;
